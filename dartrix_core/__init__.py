258600188404c8e0ff9d54ff1a9364529a6a0dd9
Daniel Adrian Ratajczyk <lacresso@gmail.com>
2026-09-06 13:06:31 +0200

    Export Rose Matrix solver API

--- dartrix_core/__init__.py
+++ dartrix_core/__init__.py
@@ -3,6 +3,7 @@ from .types import Intent, State
 from .resonance import Resonance108
 from .wolf_guardian import GateDecision, GateResult, WolfGuardian
 from .lamp import AladdinLamp
+from .matrix6x6 import Matrix6x6Error, RoseMatrix6x6Solver, SystemQuantifier, solve_rose_matrix
 
 __version__ = "0.1.0"
-__all__ = ["Intent", "State", "Resonance108", "GateDecision", "GateResult", "WolfGuardian", "AladdinLamp"]
+__all__ = ["Intent", "State", "Resonance108", "GateDecision", "GateResult", "WolfGuardian", "AladdinLamp", "Matrix6x6Error", "RoseMatrix6x6Solver", "SystemQuantifier", "solve_rose_matrix"]
